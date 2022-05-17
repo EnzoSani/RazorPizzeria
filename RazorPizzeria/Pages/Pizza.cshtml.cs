bef01dea6212b42c782e5380722a43cc76726dcb
EnzoSani <enzosanabria23@gmail.com>
2022-05-17 02:19:11 -0300

    Mock Database and sending data over asp-route

--- RazorPizzeria/Pages/Pizza.cshtml.cs
+++ RazorPizzeria/Pages/Pizza.cshtml.cs
@@ -1,10 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages
 {
     public class PizzaModel : PageModel
     {
+        public List<PizzasModel> fakePizzaDb = new List<PizzasModel>()
+        {
+            new PizzasModel(){ImageTitle="Margerita", PizzaName="Margerita", BasePrice= 2, TomatoSauce=true, Cheese=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Bolognese", PizzaName="Bolognese", BasePrice= 2, TomatoSauce=true, Cheese=true, Beef=true, Pineapple=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Hawaiian", PizzaName="Hawaiian", BasePrice= 2, TomatoSauce=true, Cheese=true, Ham=true, MushRoom=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Carbonara", PizzaName="Carbonara", BasePrice= 2, TomatoSauce=true, Cheese=true, Ham=true, Beef=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="MeatFeast", PizzaName="MeatFeast", BasePrice= 2, TomatoSauce=true, Cheese=true, Ham=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Mushroom", PizzaName="Mushroom", BasePrice= 2, TomatoSauce=true, Cheese=true, MushRoom=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Pepperoni", PizzaName="Pepperoni", BasePrice= 2, TomatoSauce=true, Cheese=true, Peperoni=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Seafood", PizzaName="Seafood", BasePrice= 2, TomatoSauce=true, Cheese=true, FinalPrice= 4 },
+            new PizzasModel(){ImageTitle="Vegetarian", PizzaName="Vegetarian", BasePrice= 2, TomatoSauce=true, Cheese=true, FinalPrice= 4 }
+           
+        };
         public void OnGet()
         {
         }
