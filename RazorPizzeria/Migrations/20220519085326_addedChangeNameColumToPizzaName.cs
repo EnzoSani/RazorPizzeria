18c4e8214a4a4588681897771fb5c8349e35db07
EnzoSani <enzosanabria23@gmail.com>
2022-05-19 06:01:00 -0300

    Save Pizza Orders

---
+++ RazorPizzeria/Migrations/20220519085326_addedChangeNameColumToPizzaName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPizzeria.Migrations
+{
+    public partial class addedChangeNameColumToPizzaName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "PizzaOrders",
+                newName: "PizzaName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PizzaName",
+                table: "PizzaOrders",
+                newName: "Name");
+        }
+    }
+}
